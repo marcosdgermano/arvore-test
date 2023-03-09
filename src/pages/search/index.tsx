39860d9bb6c127408960819b757f65aeee597e00
Marcos Germano <marcosdgermano@gmail.com>
2023-03-09 01:28:50 -0300

    creating mobile filter modal

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 import queryString from 'query-string';
 import Card from '@components/card';
 import Filters from '@components/filters';
+import FiltersModal from '@components/filters/modal';
 import { requestBooks } from '@services/books';
 import { useFilters } from '@services/filters';
 import { BookEntity } from 'types/books';
@@ -13,6 +14,7 @@ export const Search = () => {
   const [books, setBooks] = useState<BookEntity[]>([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [visible, setVisible] = useState(false);
   const targetObserverRef = useRef<HTMLSpanElement>(null);
   const { selectedFilters, allFilters } = useFilters(history.location.search);
   const searchTerm = queryString.parse(history.location.search).q as string || '';
@@ -21,7 +23,7 @@ export const Search = () => {
     if(loading) return
 
     setLoading(true);
-    const { result, error } = await requestBooks(searchTerm, { maxResults: 10, startIndex: books.length }, selectedFilters)
+    const { result, error } = await requestBooks(searchTerm, { maxResults: 40, startIndex: books.length }, selectedFilters)
 
     if (error) {
       setError(error);
@@ -37,19 +39,23 @@ export const Search = () => {
 
   useEffect(() => {
     fetch();
-  }, [searchTerm])
+  }, [history.location.search])
 
-  // useEffect(() => {
-  //   const targetObserver = targetObserverRef.current;
-  //   const intersectionObserver = new IntersectionObserver(([entry]) => {
-  //     if (entry?.isIntersecting) {
-  //       fetch(true);
-  //     }
-  //   });
+  useEffect(() => {
+    const targetObserver = targetObserverRef.current;
+    const intersectionObserver = new IntersectionObserver(([entry]) => {
+      if (entry?.isIntersecting) {
+        fetch(true);
+      }
+    });
 
-  //   targetObserver && intersectionObserver.observe(targetObserver);
-  //   return () => { targetObserver && intersectionObserver.unobserve(targetObserver) };
-  // })
+    targetObserver && intersectionObserver.observe(targetObserver);
+    return () => { targetObserver && intersectionObserver.unobserve(targetObserver) };
+  })
+
+  const cleanFilters = () => {
+    history.push(`${history.location.pathname}?${queryString.stringify({ q: searchTerm })}`);
+  }
 
   if (loading && !books.length) return <div>loading</div>;
 
@@ -63,6 +69,13 @@ export const Search = () => {
         <Filters filters={allFilters} />
       </FiltersWrapper>
       <ListWrapper>
+        <StyledButton primary onClick={() => setVisible(true)}>
+          <img src='/public/assets/stroke.png' />
+          FILTRAR
+        </StyledButton>
+        <StyledButton onClick={cleanFilters}>
+          LIMPAR FILTRO
+        </StyledButton>
         <h2>Resultados para "{ searchTerm }"</h2>
         <StyledList>
           {books.map((card, index) => (
@@ -73,9 +86,10 @@ export const Search = () => {
         </StyledList>
         { loading && <div>loading</div> }
       </ListWrapper>
-    </PageWrapper>
+      </PageWrapper>
       <span ref={targetObserverRef} style={{ width: '100%' }} />
-      </>
+      <FiltersModal filters={allFilters} visible={visible} onClose={() => setVisible(false)} />
+    </>
   );
 };
 
@@ -119,10 +133,6 @@ const ListWrapper = styled.div`
   flex-direction: column;
   flex-basis: 75%;
 
-  /* @media (max-width: 769px) {
-    flex-basis: 30%;
-  } */
-
   @media (max-width: 541px) {
     flex-basis: 100%;
     align-items: center;
@@ -140,7 +150,6 @@ const StyledList = styled.ul`
     grid-template-columns: 1fr 1fr;
     grid-column-gap: 30px;
     grid-row-gap: 30px;
-
   }
 `;
 
@@ -149,5 +158,32 @@ const StyledItem = styled.li`
     width: fit-content;
   }
 `
+
+const StyledButton = styled.button<{ primary?: boolean }>`
+  display: none;
+
+  @media (max-width: 541px) {
+    height: 60px;
+    width: 100%;
+    background: ${({ primary }) => primary ? '#8553F4' : '#ADB7BF'};
+    border: 1px solid rgba(64, 106, 118, 0.2);
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 15px;
+
+    color: #F1F7FC;
+    font-weight: bold;
+    opacity: 0.6;
+
+    & > span {
+      margin-left: 30px;
+    }
+    & > img {
+      padding-right: 15px;
+    }
+  }
+`;
 
 export default Search;
